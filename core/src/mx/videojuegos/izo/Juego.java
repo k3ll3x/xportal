bf2817ae108476ac9f938a661f9f863c720e033f
isain <isain_5@hotmail.com>
2016-10-06 16:35:49 -0500

    Hacker Isain

--- core/src/mx/videojuegos/izo/Juego.java
+++ core/src/mx/videojuegos/izo/Juego.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Game;
  * Created by isain on 9/5/2016.
  *
  * Sistema de control de versiones
+ * ejercicio isain
  */
 public class Juego extends Game {
     public void create() {
