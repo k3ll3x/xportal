9b5278aee70534ecd5827049e2add7cbfc1ca311
gabsh26 <A01375242@itesm.mx>
2016-10-10 19:29:46 -0500

    clases

--- core/src/mx/videojuegos/izo/nivel1/MiniJuego1.java
+++ core/src/mx/videojuegos/izo/nivel1/MiniJuego1.java
@@ -6,7 +6,7 @@ package mx.videojuegos.izo.nivel1;
  * El primer mini-juego consiste:
  * ´Catch de Potion'
  *
- * Objetos caen del cielo, el personaje tiene que recoger x numero de objetos o alcanzar
+ * Objetos(pocimas) caen del cielo, el personaje tiene que recoger x numero de objetos o alcanzar
  * cierto score, debe cnservar sus vidas o hacerlo dentro del intervalo del tiempo para lograr ganar-pasar de nivel,
  * si no cumple con los requisitos, tendra que volver a jugar el nivel
  *
