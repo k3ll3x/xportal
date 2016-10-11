342b6c8309324e85a5567025661c9a236ef584ed
gabsh26 <A01375242@itesm.mx>
2016-10-10 19:28:13 -0500

    comentarios

---
+++ core/src/mx/videojuegos/izo/nivel1/Arma.java
@@ -0,0 +1,18 @@
+package mx.videojuegos.izo.nivel1;
+
+/**
+ * Created by Gab on 10/10/2016.
+ *
+ * Posibles Estados:
+ * Recogida
+ * No recogida
+ * Atacando
+ * Oculta
+ *
+ * debe tener un contador
+ * Posicion
+ * Cambiar animacion
+ *
+ */
+public class Arma {
+}
---
+++ core/src/mx/videojuegos/izo/nivel1/mapaNivel.java
@@ -0,0 +1,12 @@
+package mx.videojuegos.izo.nivel1;
+
+/**
+ * Created by Gab on 10/10/2016.
+ *
+ * El mapa debe:
+ * detectar las colisiones en plataformas
+ * respetar limite de paredes
+ *
+ **/
+public class mapaNivel {
+}
